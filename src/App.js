00c6154f297a8101d3a8e0cd2874847a4ef79055
ashutosh <ashumantoo@gmail.com>
2020-02-27 19:59:27 +0530

    lists , loop and passing the state throught the props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Navbar from './components/navbar/Navbar'
-import UserItem from './components/Users/UserItem';
+import Users from './components/Users/Users';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,9 @@ class App extends Component {
     return (
       <div className="App" >
         <Navbar icon="fab fa-github" title="Github-finder" />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -16,21 +16,30 @@ class UserItem extends Component {
         }
     }
     ============================================================================*/
-    state = {
-        user: {
-            id: 1,
-            login: "mojombo",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo",
-        }
-    }
+    //===========================================================================
+    // Now Passing this local component state data from the props i.e 
+    // now this state data gets passed from the users component to the 
+    // userItem component using props
+    //
+    // state = {
+    //     user: {
+    //         id: 1,
+    //         login: "mojombo",
+    //         avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    //         html_url: "https://github.com/mojombo",
+    //     }
+    // }
+    //===========================================================================
 
     render() {
+        //destructing the this.props.user because we used 'this.props.user' lots of 
+        //the places in the code
+        // const { login, avatar_url, html_url } = this.props.user;
         return (
             <div className="card text-center">
-                <img className="round-img" src={this.state.user.avatar_url} alt="user_logo" style={{ width: "60px" }} />
-                <h4>{this.state.user.login}</h4>
-                <a href={this.state.user.html_url} className="btn btn-dark btn-sm my-1">More</a>
+                <img className="round-img" src={this.props.user.avatar_url} alt="user_logo" style={{ width: "60px" }} />
+                <h4>{this.props.user.login}</h4>
+                <a href={this.props.user.html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
         )
     }
