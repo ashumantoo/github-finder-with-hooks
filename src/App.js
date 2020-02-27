bfb2658477fa2efb49005c19a53b9dfb8a32a596
ashutosh <ashumantoo@gmail.com>
2020-02-27 22:14:49 +0530

    making http call using axios and using state in the app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Navbar from './components/navbar/Navbar'
 import Users from './components/Users/Users';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  //Promise Based axios calls
+  //
+  // componentDidMount() {
+  //   this.setState({ loading: true });
+  //   axios
+  //     .get('https://api.github.com/users')
+  //     .then(res => {
+  //       console.log(res.data);
+  //       this.setState({ users: res.data });
+  //       this.setState({ loading: false });
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
+  // }
+
+  //Async await type axios call
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ loading: false, users: res.data });
+  }
+
   render() {
     return (
       <div className="App" >
         <Navbar icon="fab fa-github" title="Github-finder" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
