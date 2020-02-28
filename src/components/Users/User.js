990f856423e5346e838ce43e6ff45ebef633b34b
ashutosh <ashumantoo@gmail.com>
2020-02-28 22:18:27 +0530

    user component ui and layout

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import Spinner from '../Layout/Spinner';
 
 class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
     }
+
+    static propsTypes = {
+        getUser: PropTypes.func.isRequired,
+        loading: PropTypes.bool.isRequired,
+        user: PropTypes.object.isRequired,
+    }
+
     render() {
         const {
             name,
             avatar_url,
             location,
+            company,
             bio,
             blog,
             login,
@@ -22,10 +34,58 @@ class User extends Component {
         } = this.props.user;
 
         const { loading } = this.props;
+        if (loading) return <Spinner />
+
         return (
-            <div>
-                {name}
-            </div>
+            <Fragment>
+                <Link to='/' className="btn btn-light">Back To Search </Link>
+                Hireable: {' '}
+                {hireable ? (<i className="fas fa-check text-success" />) :
+                    (<i className="fas fa-times-circle text-danger" />)}
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} alt="" className="round-img" style={{ width: "150px" }} />
+                        <h1>{name}</h1>
+                        <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && (<Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>)}
+                        <a href={html_url} className="btn btn-dark">Visit Github Profile</a>
+                        <ul>
+                            <li>
+                                {login && (
+                                    <Fragment>
+                                        <strong>Username: </strong>{login}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {company && (
+                                    <Fragment>
+                                        <strong>Company: </strong>{company}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {blog && (
+                                    <Fragment>
+                                        <strong>Website: </strong>{blog}
+                                    </Fragment>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card text-center">
+                    <div className="badge badge-primary">Followers: {followers}</div>
+                    <div className="badge badge-light">Following: {following}</div>
+                    <div className="badge badge-dark">Public Repos: {public_repos}</div>
+                    <div className="badge badge-success">Public Gists: {public_gists}</div>
+                </div>
+            </Fragment>
         )
     }
 }
